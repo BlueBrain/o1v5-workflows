f661fb6fb204b64e61067bc33c9b81d09d87621b
Christoph Pokorny <christoph.pokorny@epfl.ch>
2023-01-13 09:45:18 +0100

    Added manipulation sbatch script

---
+++ blob_stim_replication/configs/run_manip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --job-name=conn_manip
+#SBATCH --time=8:00:00
+#SBATCH --account=proj9
+#SBATCH --partition=prod
+#SBATCH --mem=0
+#SBATCH --exclusive
+#SBATCH --constraint=cpu
+
+module purge
+module load archive/2021-07
+source /gpfs/bbp.cscs.ch/home/pokorny/ToposampleKernel/bin/activate
+python -u -m connectome_manipulator.connectome_manipulation $1 $2 $3 $4
+
+# EXAMPLE HOW TO RUN: sbatch run_manip.sh <manip_config.json> [do_profiling] [do_resume] [keep_parquet]
+# e.g. sbatch run_manip.sh manip_config.json 1 0 0
